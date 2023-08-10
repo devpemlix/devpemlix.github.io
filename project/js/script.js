9fbb9e6369df5c7b2c35bd258fe4799adafa2dc5
devpemlix <139529865+devpemlix@users.noreply.github.com>
2023-08-10 16:08:22 +0530

    update

---
+++ project/js/script.js
@@ -0,0 +1,15 @@
+
+
+
+document.getElementById("cookies-button-1").onclick = function() {
+    document.getElementById("cookies-section").style.display = "none";
+}
+document.getElementById("cookies-button-2").onclick = function() {
+    document.getElementById("cookies-section").style.display = "none";
+}
+document.getElementById("cross").onclick = function() {
+    document.getElementById("cookies-section").style.display = "none";
+   
+}
+
+
