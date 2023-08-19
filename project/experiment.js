5d62f25f600f62cf040be6fa920f2d1f1571e1a5
devpemlix <139529865+devpemlix@users.noreply.github.com>
2023-08-19 18:05:56 +0530

    update

--- project/experiment.js
+++ project/experiment.js
@@ -1,8 +1,11 @@
-var btn =document.querySelectorAll(".item")
-var i=0;
-btn[0].addEventListener("click",function(){
-    btn[0].style.background ="purple";
-})
+
+
+
+
+
+
+
+
 
 
 
--- project/js/script.js
+++ project/js/script.js
@@ -1,14 +1,33 @@
+var section = document.getElementById("cookies-section");
+
+
+var current_location = location.href;
+var btn_item =document.querySelectorAll(".btn");
+var btn_length = btn_item.length;
+for(var i=0;i<btn_length;i++){
+    if(btn_item[i].href === current_location){
+        btn_item[i].className = "active";
+    }
+}
+
+ 
+
+function hide(){
+    section.style.display = "none";   
+}
+
 
 document.getElementById("cookies-button-1").onclick = function() {
-    document.getElementById("cookies-section").style.display = "none";
+    hide();
 }
 document.getElementById("cookies-button-2").onclick = function() {
-    document.getElementById("cookies-section").style.display = "none";
+    hide();
 }
 document.getElementById("cross").onclick = function() {
-    document.getElementById("cookies-section").style.display = "none";
-   
+    hide(); 
 }
+
+
 
 
 
