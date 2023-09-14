ffb5de9bddce9f1b76b62c72432b77a8022517b8
devpemlix <139529865+devpemlix@users.noreply.github.com>
2023-09-14 19:32:58 +0530

    update

--- project/protalk-website/script.js
+++
@@ -1,4 +0,0 @@
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('.btn-large'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
