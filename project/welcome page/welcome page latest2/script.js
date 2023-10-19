d79dc4b8339ea3c162fe89cf0f415ef02865d346
devpemlix <139529865+devpemlix@users.noreply.github.com>
2023-10-19 19:27:58 +0530

    update

--- project/welcome page/welcome page latest2/script.js
+++ project/welcome page/welcome page latest2/script.js
@@ -1,28 +1,24 @@
 let btn_admin = document.querySelector(".btn-admin");
 let btn_agent = document.querySelector(".btn-agent");
 let button_box = document.querySelector(".button-link");
-
+let agent_box = document.querySelector(".agent-box");
+let admin_box = document.querySelector(".admin-box");
 
 let agent_login_input = document.querySelector(".agent-login-input");
 let admin_login_input = document.querySelector(".admin-login-input");
 
-btn_agent.style.backgroundColor = "#04BF8A";
 
 btn_admin.addEventListener("click",function(){
+    agent_box.style.display = "none";
+    admin_box.style.display = "none";
     admin_login_input.style.display = "block";
-    agent_login_input.style.display = "none";
-    btn_admin.style.backgroundColor = "#04BF8A";
-    btn_agent.style.backgroundColor = "#0099ff";
-   
 
 })
 
 btn_agent.addEventListener("click",function(){
+    agent_box.style.display = "none";
+    admin_box.style.display = "none";
     agent_login_input.style.display = "block";
-    admin_login_input.style.display = "none";
-    btn_admin.style.backgroundColor = "#0099ff";
-    btn_agent.style.backgroundColor = "#04BF8A"
-   
 
 })
 
